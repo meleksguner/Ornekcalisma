1d10d77d6b953ba3d6beb5d81f0ac2fa022325ab
meleksguner <msenaguner34@gmail.com>
2024-11-09 16:29:09 +0300

    Add files via upload

---
+++ çarpma do-while.cs
@@ -0,0 +1,22 @@
+﻿int carp = 1;
+int giris;
+
+do
+{
+    giris = Convert.ToInt32(Console.ReadLine());
+    carp *= giris;
+    
+}
+while (giris!=1);
+Console.WriteLine(carp);
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
