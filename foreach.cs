473f9a91e18b34cc0942fdefea473ce8dc8b6b92
Melek <msenaguner34@gmail.com>
2024-11-09 17:18:23 +0300

    Add files via upload

---
+++ foreach.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        // List oluşturma ve tamsayı ekleme
+        ArrayList sayilar = new ArrayList { 10, 20, 30, 40, 50 };
+
+        int toplam = 0;
+
+        // foreach döngüsü ile sayıları yazdırma ve toplamı hesaplama
+        foreach (int sayi in sayilar)
+        {
+            Console.WriteLine("Sayı: " + sayi);
+            toplam += sayi;
+        }
+
+        // Toplamı ekrana yazdırma
+        Console.WriteLine("Sayıların Toplamı: " + toplam);
+    }
+}
+
